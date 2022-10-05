c646ebc65d42155d4057b1ad42267eb145fd7467
Sandaime-As <asenov1998@mail.ru>
2022-10-05 23:38:41 +0600

    решили две общие задачи

---
+++ task22.1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 22:**
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4*/
+using static System.Console;
+Clear();
+Write("enter the number: ");
+int number=Convert.ToInt32(ReadLine());
+int count=1;
+while (count<=number)
+{
+    Write($"{Math.Pow(count,2)} ");
+    count++;
+}
---
+++ task24/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+3 -> Среда
+5 -> Пятница*/
+using static System.Console;
+Clear();
+Write("Введите номер дня недели: ");
+int number=Convert.ToInt32(ReadLine());
+switch (number)
+{
+    case 1:
+    {
+        WriteLine("Понедельник");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("Вторник");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("Среда");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("Четверг");
+        break;
+    }
+    case 5:
+    {
+        WriteLine("Пятница");
+        break;
+    }
+    case 6:
+    {
+        WriteLine("Суббота");
+        break;
+    }
+    case 7:
+    {
+        WriteLine("Воскресенье");
+        break;
+    }
+    default:
+    {
+        WriteLine("Номер дня недели варьируется от 1 до 7");
+        break;
+    }
+}
